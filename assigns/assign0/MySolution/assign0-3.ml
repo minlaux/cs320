e12a37c916ce744dd3a27e98be03b0ff8e380120
Polina <93955709+minlaux@users.noreply.github.com>
2023-09-11 13:27:04 -0400

    modified assign0-3.ml and assign0-4.ml

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -15,42 +15,23 @@ fun int2str(i0: int): string
 
 (* converting int to string from an int to char list *)
 
-
 let int2str (i0: int): string =
 
-let rec int2char (n : int) (acc : char list) =
-if n < 10 then 
-Char.chr (Char.code '0' + n) :: acc
+let rec int2char n acc =
+if n < 10 then Char.chr (Char.code '0' + n) :: acc
 
-else 
+else
 let num = n mod 10 in
-
 let num_char = Char.chr (Char.code '0' + num) in
-
 int2char (n / 10) (num_char :: acc) in
 
-let chars = 
+let chars =
 if i0 = 0 then ['0']
 
-else if i0 < 0 then 
-'_' :: (int2char (-i0) [])
+else if i0 < 0 then '-' :: int2char (-i0) []
 
-else int2char i0 [] in 
-
-let len = List.length chars in
-
-let str_chars = Array.make len ' ' in 
-
-let rec fill_array i chars =
-match chars with 
-| [] -> ()
-| c :: rest ->
-str_chars.(i) <- c;
-fill_array(i + 1) rest in
-
-fill_array 0 chars;
-
-String.init len (fun i -> str_chars.(i))
+else int2char i0 [] in
+String.init (List.length chars) (fun i -> List.nth chars i)
 
 ;;
 
--- assigns/assign0/MySolution/assign0-4.ml
+++ assigns/assign0/MySolution/assign0-4.ml
@@ -25,27 +25,22 @@ if index < 0 then acc
 
 else if is_num cs.[index] then
 let num_value = Char.code cs.[index] - Char.code '0' in
-
-let new_acc = acc + num_value * int_of_float (10.0 ** float_of_int (String.length cs - index - 1)) in 
+let power = String.length cs - index - 1 in
+let new_acc = acc + num_value * int_of_float (10.0 ** float_of_int power) in
 convert new_acc (index - 1)
 
 else
 failwith "Invalid input: not a valid integer string" in
-
 let length = String.length cs in
-
 if length = 0 then
 failwith "Empty input: not a valid integer string"
 
-else if cs.[0] = '-' then 
-- (convert 0 (length - 1))
+else if cs.[0] = '-' then - (convert 0 (length - 1))
 
-else if cs.[0] = '+' then 
+else if cs.[0] = '+' then convert 0 (length - 1)
+
+else
 convert 0 (length - 1)
-
-else convert 0 (length - 1)
-
-;;
 
 
 (* ****** ****** *)
