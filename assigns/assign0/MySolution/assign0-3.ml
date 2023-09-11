192a6605cbeeab37334d830cfd01cdf02f49dc1b
Polina <93955709+minlaux@users.noreply.github.com>
2023-09-11 11:20:02 -0400

    finished assign0-4.ml

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -15,24 +15,30 @@ fun int2str(i0: int): string
 
 (* converting int to string from an int to char list *)
 
+
 let int2str(i0: int): string =
 
 let rec int2char (n : int) (acc : char list) =
 if n < 10 then 
 Char.chr (Char.code '0' + n) :: acc
+
 else 
 let num = n mod 10 in
+
 let num_char = Char.chr (Char.code '0' + num) in
 
 int2char (n / 10) (num_char :: acc) in
 
 let chars = 
 if i0 = 0 then ['0']
+
 else if i0 < 0 then 
 '_' :: (int2char (-i0) [])
+
 else int2char i0 [] in 
 
 let len = List.length chars in
+
 let str_chars = Array.make len ' ' in 
 
 let rec fill_array i chars =
--- assigns/assign0/MySolution/assign0-4.ml
+++ assigns/assign0/MySolution/assign0-4.ml
@@ -5,3 +5,47 @@ string to an integer:
 fun str2int(cs: string): int
 In particular, it is expected that str2int(int2str(x)) = x
 *)
+
+
+(* ****** ****** *)
+
+
+#use "./../assign0.ml"
+;;
+
+
+(* converting string to int from an int to char list *)
+
+
+let str2int (cs: string): int =
+let is_num c = Char.code '0' <= Char.code c && Char.code c <= Char.code '9' in
+
+let rec convert acc index =
+if index < 0 then acc
+
+else if is_num cs.[index] then
+let num_value = Char.code cs.[index] - Char.code '0' in
+
+let new_acc = acc + num_value * int_of_float (10.0 ** float_of_int (String.length cs - index - 1)) in 
+convert new_acc (index - 1)
+
+else
+failwith "Invalid input: not a valid integer string" in
+
+let length = String.length cs in
+
+if length = 0 then
+failwith "Empty input: not a valid integer string"
+
+else if cs.[0] = '-' then 
+- (convert 0 (length - 1))
+
+else if cs.[0] = '+' then 
+convert 0 (length - 1)
+
+else convert 0 (length - 1)
+
+;;
+
+
+(* ****** ****** *)
