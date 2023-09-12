58f5371197153ba58d5ce0f87eee334d31c9dca1
Polina <93955709+minlaux@users.noreply.github.com>
2023-09-12 19:01:16 -0400

    rewrote assign0-5.ml to adhere to hw guidelines

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -13,43 +13,20 @@ fun stringrev(cs: string): string
 ;;
 
 
-(* assigns each character in string to an array index in reverse order *)
+(*
+recursevly reverses input string
+finds index of last character to place first when initialising string
+*)
 
-let char_arr (str : string): char array =
-let str_len = String.length str in
-let arr = Array.make str_len ' ' in
-let inc = ref 0 in
-
-for i = str_len - 1 downto 0 do
-
-let char_at_i = String.get str i in
-arr.(!inc) <- char_at_i;
-inc := !inc + 1;
-
-done;
-
-arr
+let stringrev(cs: string): string =
+let len = String.length cs in
+let reversed = String.init len (fun i ->
+let index = len - i - 1 in
+String.get cs index) in
+reversed
 
 ;;
 
-(* converts character array to string *)
-
-let arr2str (ch : char array): string =
-let length = Array.length ch in
-String.init length (fun i -> ch.(i))
-
-;;
-
-
-(* returns input string in reverse order *)
-
-let stringrev (cs : string): string =
-if String.length cs = 0 then ""
-
-else
-arr2str(char_arr (cs))
-
-;;
 
 
 (* ****** ****** *)
