b99f604c85b87bc94dd8b75d3f030a62d567d2ee
Polina <93955709+minlaux@users.noreply.github.com>
2023-09-19 13:55:03 -0400

    changes to assign0-3.ml

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -13,6 +13,20 @@ fun int2str(i0: int): string
 ;;
 
 
+(*
+appends character c to begining of string s
+*)
+
+let combine_str_char (s : string) (c : char): string =
+    let len_s = string_length s in
+    let result = string_init (len_s + 1) (fun i ->
+        if i = 0 then c
+
+        else 
+            string_get (s, (i - 1))) in
+        result
+
+
 (* 
 converting integer to string value of the integer 
 base case: one-digit integer; return: string of int
@@ -21,9 +35,16 @@ builds string from remainders
 *)
 
 let rec int2str (i0: int): string =
-    if i0 < 10 then 
+    if i0 < 0 then 
+        let new_i0 = i0 * (-1) in
+        let new_str = int2str (new_i0) in
+        let neg = chr (ord '-') in
+        combine_str_char new_str neg
+    
+    else if i0 < 10 then 
         let result = chr (ord '0' + i0) in
         string_init 1 (fun i -> result)
+    
     else
         let num = i0 mod 10 in
         let num_ch = chr (ord '0' + num) in
@@ -37,5 +58,6 @@ let rec int2str (i0: int): string =
                 else
                     string_get (num_str, (i - len_r)))
 ;;
+
 
 (* ****** ****** *)
