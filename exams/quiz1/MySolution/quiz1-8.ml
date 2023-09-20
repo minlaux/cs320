f982af1b38ebef2c29acca9c7cc98091c9379a83
Polina <93955709+minlaux@users.noreply.github.com>
2023-09-20 17:39:42 -0400

    fixed quiz1-8.ml

--- exams/quiz1/MySolution/quiz1-8.ml
+++ exams/quiz1/MySolution/quiz1-8.ml
@@ -17,35 +17,75 @@
 *)
 
 
+(*
+sorts a tuple of 2 integers
+*)
 
-let sort_below (x, y, z) =
-    if x < y then 
-        if x < z then x 
-        else z 
-    else if y < z then y 
-    else z
-
-
-let sort_above (x, y, z) =
-    if x > y then 
-        if x > z then x 
-        else z 
-    else if y > z then y 
-    else z
+let sort2 (a, b) =
+  if a < b then (a, b)
+  else (b, a)
 
 
-let sort2 (x, y) =
-  if x < y then (x, y)
-  else (y, x)
+(*
+sorts a tuple of 3 integers
+*)
 
+let sort3 (a, b, c) =
+    if a <= b && a <= c then
+        if b <= c then 
+            (a, b, c)
+        else 
+            (a, c, b)
+    
+    else if b <= a && b <= c then
+        if a <= c then 
+            (b, a, c)
+        else 
+            (b, c, a)
+    
+    else
+        if a <= b then 
+            (c, a, b)
+        else 
+            (c, b, a)
+
+
+(*
+sorts a tuple of 4 integers
+*)
+
+let sort4 (a, b, c, d) =
+    let (a, b) = sort2 (a, b) in
+    let (a, c) = sort2 (a, c) in
+    let (a, d) = sort2 (a, d) in
+
+    let (b, c) = sort2 (b, c) in
+    let (b, d) = sort2 (b, d) in
+
+    let (c, d) = sort2 (c, d) in
+
+    let (a, b, c) = sort3 (a, b, c) in
+    let (a, b, d) = sort3 (a, b, d) in
+    let (a, c, d) = sort3 (a, c, d) in
+
+    let (b, c, d) = sort3 (b, c, d) in
+    (a, b, c, d)
+
+
+(*
+sorts a tuple of 5 integers
+*)
 
 let sort5 (a, b, c, d, e) =
-    let (n0, n4) = sort2 (a, e) in
-    let (n1, n3) = sort2 (b, d) in
-    let n2 = sort_below (c, n3, n4) in
-    let min = sort_below (n0, n1, n2) in
-    let max = sort_above (n0, n1, n2) in
-    (min, n0, n1, n2, max)
+    let (a, b, c, d) = sort4 (a, b, c, d) in
+    let (a, b, c, e) = sort4 (a, b, c, e) in
+    let (a, b, d, e) = sort4 (a, b, d, e) in
+    let (a, c, d, e) = sort4 (a, c, d, e) in
+
+    let (b, c, d, e) = sort4 (b, c, d, e) in
+
+    let (a, b, c, d, e) = sort5 (a, b, c, d, e) in
+    (a, b, c, d, e)
     
 ;;
 
