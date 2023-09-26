4d11337768737c33220129a4fe25acd2dabdfb06
Polina <93955709+minlaux@users.noreply.github.com>
2023-09-26 13:41:11 -0400

    finished assign2-1.ml

--- assigns/assign2/MySolution/OCaml/assign2-1.ml
+++ assigns/assign2/MySolution/OCaml/assign2-1.ml
@@ -15,20 +15,23 @@ mylist_length(xs: 'a mylist): int = ...
 (* ****** ****** *)
 
 
-#use "./../assign2.ml"
+#use "./../../assign2.ml"
 ;;
 
-#use "./../../../classlib/OCaml/MyOcaml.ml"
+#use "./../../../../classlib/OCaml/MyOcaml.ml"
 ;;
+
 
 
 let rec mylist_length(xs: 'a mylist): int =
+    match xs with
+    | MyNil -> 0
+    | MyCons(_, tail) -> 1 + mylist_length(tail)
+    | MySnoc(head, _) -> 1 + mylist_length(head)
+    | MyReverse (x0) -> mylist_length(x0) 
+    | MyAppend2(xs1, xs2) -> mylist_length(xs1) + mylist_length(xs2)
 
-
-
-
-
-
+;;
 
 
 (* ****** ****** *)
