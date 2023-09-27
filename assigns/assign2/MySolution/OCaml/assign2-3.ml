fe5912d47d9d8e4dbc6356a75d7dbda91f86e720
Polina <93955709+minlaux@users.noreply.github.com>
2023-09-27 11:10:30 -0400

    working on assign2-3.ml

--- assigns/assign2/MySolution/OCaml/assign2-3.ml
+++ assigns/assign2/MySolution/OCaml/assign2-3.ml
@@ -21,16 +21,34 @@ foldleft_to_iforeach
 
 let foldleft_to_iforeach 
 (foldleft: ('xs, 'x0, int) foldleft): ('xs, 'x0) iforeach =
-  
-    match xs with
+    
+    let iforeach (list: 'xs mylist) (work: 'x0 -> unit): unit =
+        
+        let rec fold_fn(acc)(element) =
+            let new_acc = foldleft list acc (fun a b -> b) in
+            work element;
+            
+            match new_acc with
+            | Some acc' -> fold_fn acc' element
+            | None -> ()
+            in
+
+            let () = fold_fn None None in ()
+        in
+
+        iforeach
+
+;;
+
+
+
+(*
+ match xs with
     | MyNil -> ()
     | MyCons() -> 
     | MySnoc() ->
     | MyReverse() ->
     | MyAppend2() ->
-    
-;;
-
-
+*)
 
 (* ****** ****** *)
