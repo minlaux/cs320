6f14344e52374788294e5d456b7d545c5d2f20b8
Polina <93955709+minlaux@users.noreply.github.com>
2023-10-04 19:38:46 -0400

    finished assign3

--- assigns/assign3/MySolution/OCaml/assign3-5.ml
+++ assigns/assign3/MySolution/OCaml/assign3-5.ml
@@ -30,7 +30,80 @@ returns a list of boards consisting of all the solutions to the puzzle.
 #use "./../../../../classlib/OCaml/MyOcaml.ml"
 ;;
 
+let N = 8 in 
 
+
+let rec print_dots i =
+    if i > 0 then (
+    print_dots (i - 1)
+  )
+
+
+let print_row i =
+    print_dots i;
+    print_string "Q ";
+    print_dots (N - i - 1);
+    print_newline ()
+
+
+let board_get bd i =
+    List.nth bd i
+
+
+let board_set bd i j =
+    let rec update_list lst i j =
+        match lst with
+        | [] -> []
+        | hd :: tl ->
+            if i = 0 then j :: tl
+            else hd :: update_list tl (i - 1) j
+    
+    in
+    update_list bd i j
+
+
+let safety_test1 i0 j0 i j =
+    j0 <> j && abs (i0 - i) <> abs (j0 - j)
+
+
+let safety_test2 i0 j0 bd i =
+    let rec check_safety i =
+        if i >= 0 then (
+            if safety_test1 i0 j0 i (board_get bd i) then
+                check_safety (i - 1)
+            else false
+    )   else true
+    
+    in
+    check_safety (i - 1)
+
+
+let rec search bd i j nsol =
+    if j < N then (
+        let test = safety_test2 i j bd (i - 1) in
+        if test then (
+            let bd1 = board_set bd i j in
+            if i + 1 = N then (
+                search bd i (j + 1) (nsol + 1)
+      ) else (
+            search bd1 (i + 1) 0 nsol
+      )
+    ) else (
+        search bd i (j + 1) nsol
+    )
+  ) else (
+        if i > 0 then (
+            search bd (i - 1) (board_get bd (i - 1) + 1) nsol
+        ) else nsol
+  )
+
+
+let queen8_puzzle_solve () =
+    let bd = [0; 0; 0; 0; 0; 0; 0; 0] in
+    search bd 0 0 0
+
+
+;;
 
 
 (* ****** ****** *)
--- assigns/assign3/MySolution/Python/assign3_6.py
+++ assigns/assign3/MySolution/Python/assign3_6.py
@@ -2,72 +2,44 @@ import sys
 sys.path.append("./../../../../classlib/Python")
 from MyPython import *
 
+
 # Assign3-6: 30 points
 # Please translate the datatype mylist (given in Assign2) into
 # type classes (by following the example of fnlist in MyPython.py).
 # Then please translate mylist_foreach and mylist_rforeach into Python
 
-"""
-type 'a mylist =
-  | MyNil
-  | MyCons of 'a * 'a mylist
-  | MySnoc of 'a mylist * 'a
-  | MyReverse of 'a mylist
-  | MyAppend2 of 'a mylist * 'a mylist
-"""
-
-class MyNil:
-    def __init__(self):
-        self.tag = "MyNil"
-
-class MyCons:
-    def __init__(self, head, tail):
-        self.tag = "MyCons"
-        self.head = head
-        self.tail = tail
-
-class MySnoc:
-    def __init__(self, tail, last):
-        self.tag = "MySnoc"
-        self.tail = tail
-        self.last = last
-
-class MyReverse:
-    def __init__(self, xs):
-        self.tag = "MyReverse"
-        self.xs = xs
-
-class MyAppend2:
-    def __init__(self, xs1, xs2):
-        self.tag = "MyAppend2"
-        self.xs1 = xs1
-        self.xs2 = xs2
-
 
 
 class MyList:
-    def __init__(self, mynil, mycons, mysnoc, myreverse, myappend2):
-        self.mynil = MyNil(self)
-        self.mycons = MyCons(self)
-        self.mysnoc = MySnoc(self)
-        self.myreverse = fnlist_reverse(self)
-        self.myappend2 = MyAppend2(self)
+    """
+        OCaml mylist type to Python class MyList
+    """
+
+    def mynil(self):
+        return MyNil(self)
+    
+    def mycons(self):
+        return MyCons(self)
+    
+    def mysnoc(self):
+        return MySnoc(self)
+
+    def myreverse(self):
+        return MyReverse(self)
+
+    def myappend2(self):
+        return MyAppend2(self)
 
 
 class MyNil(MyList):
     def __init__(self):
-        def is_empty(self):
-        return True
+        pass
 
 
 class MyCons(MyList):
     def __init__(self, head, tail):
         self.head = head
         self.tail = tail
-    def get_head(self):
-        return self.head
-    def get_tail(self):
-        return self.tail
 
 
 class MySnoc(MyList):
@@ -87,32 +59,23 @@ class MyAppend2(MyList):
         self.xs2 = xs2
 
 
+def mylist_foreach(xs, work):
+    match_dict = {
+        MyNil: lambda: None,
+        MyCons: lambda: mylist_foreach(xs[1], work) or work(xs[0]),
+        MySnoc: lambda: mylist_foreach(xs[0], work) or work(xs[1]),
+        MyReverse: lambda: mylist_rforeach(xs[0], work),
+        MyAppend2: lambda: mylist_foreach(xs[0], work) or mylist_foreach(xs[1], work),
+    }
+    return match_dict[type(xs)]()
 
-def mylist_foreach(self, work):
-    if MyNil(self):
-        pass
-    elif MyCons(self):
-    self.mycons = MyCons(self)
-    self.mysnoc = MySnoc(self)
-    self.myreverse = mylist_rforeach(self, work)
-    self.myappend2 = mylist_foreach(self, work)
 
-
-    return
-
-if isinstance(xs, MyNil):
-        pass
-    elif isinstance(xs, MyCons):
-        work(xs.head)
-        mylist_foreach(xs.tail, work)
-    elif isinstance(xs, MySnoc):
-        mylist_foreach(xs.tail, work)
-        work(xs.last)
-    elif isinstance(xs, MyReverse):
-        mylist_rforeach(xs.original, work)
-    elif isinstance(xs, MyAppend2):
-        mylist_foreach(xs.list1, work)
-        mylist_foreach(xs.list2, work)
-
-def mylist_rforeach(self, work):
-    return
+def mylist_rforeach(xs, work):
+    match_dict = {
+        MyNil: lambda: None,
+        MyCons: lambda: work(xs[0]) or mylist_rforeach(xs[1], work),
+        MySnoc: lambda: mylist_rforeach(xs[0], work) or work(xs[1]),
+        MyReverse: lambda: mylist_foreach(xs[0], work),
+        MyAppend2: lambda: mylist_rforeach(xs[1], work) or mylist_rforeach(xs[0], work),
+    }
+    return match_dict[type(xs)]()
