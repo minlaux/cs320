65b2a42b34d27dbdd188ed944c68c76f40ba2748
Polina <93955709+minlaux@users.noreply.github.com>
2023-10-04 14:00:12 -0400

    working on assign3

--- assigns/assign3/MySolution/OCaml/assign3-3.ml
+++ assigns/assign3/MySolution/OCaml/assign3-3.ml
@@ -27,11 +27,52 @@ and [2;1] are DIFFERENT.
 #use "./../../../../classlib/OCaml/MyOcaml.ml"
 ;;
 
+(*
+list_map function from lecture
+*)
+
+let rec
+list_map
+(xs: 'a list)(fopr: 'a -> 'b): 'b list =
+match xs with
+| [] -> []
+| x1 :: xs -> fopr(x1) :: list_map(xs)(fopr)
+
+
+(*
+returns subsets of a given set list 
+from assign3-2.ml
+*)
+
+let list_subsets (xs: 'a list): 'a list list =
+    let initial = [[]] in
+    let fopr acc x =
+        let mapped = list_map(acc)(fun sub -> x :: sub) in
+        list_append(acc)(mapped)
+    in
+    list_foldleft xs initial fopr
+
 
 let list_nchoose(xs: 'a list)(n0: int): 'a list list =
 
+    let len = foreach_to_length(list_foreach(xs)(fun i -> 
+        list_foldleft(xs)
+        ))
+    let map_len = list_map len in 
 
+(*  
+List.map (foreach_to_length list_foreach) subsets
 
-
+    let combine acc x =
+        let append_x_to_subsets subset =
+            if List.length subset < n then
+                (x :: subset) :: acc
+            else
+                subset :: acc
+        in
+        List.fold_left append_x_to_subsets [] acc
+    in
+    list_foldleft xs [[]] combine
+*) 
 
 (* ****** ****** *)
---
+++ assigns/assign3/MySolution/OCaml/assign3-4.ml
@@ -0,0 +1,35 @@
+(*
+Assign3-4:
+HX-2023-09-26: 20 points
+Given a word x of length n, another word is a buddy
+of x if x and y differ exactly at one position. For
+instance, "live" is a buddy "love" (and "love" is also
+a buddy of "live").
+//
+Please give a NON-RECURSIVE implementation of
+list_of_buddies that returns a list of all the buddies
+of a given word.
+//
+let
+list_of_buddies(word: string): string list = ...
+
+(*
+FYI. The concept of word buddies is used in the following game:
+https://xanadu-lang.github.io/xats2js/docgen/CodeBook/Doublet/2020-11-29/
+https://github.com/xanadu-lang/xats2js/tree/master/docgen/CodeBook/Doublet
+*)
+*)
+
+(* ****** ****** *)
+
+
+#use "./../../assign3.ml"
+;;
+
+#use "./../../../../classlib/OCaml/MyOcaml.ml"
+;;
+
+
+
+
+(* ****** ****** *)
