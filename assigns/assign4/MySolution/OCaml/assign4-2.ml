2fe5bac6b2a77b0c64ad4a02002ff34b6b8efa64
Polina <93955709+minlaux@users.noreply.github.com>
2023-10-07 01:09:11 -0400

    added assign4 files

---
+++ assigns/assign4/MySolution/OCaml/assign4-2.ml
@@ -0,0 +1,27 @@
+(*
+//
+Assign4-2:
+//
+HX-2023-10-05: 10 points
+//
+Please enumerate all the pairs of natural
+numbers. Given pairs (i1, j1) and (i2, j2),
+(i1, j1) should be enumerated ahead of (i2, j2)
+if i1+j1 < i2+j2.
+//
+let theNatPairs: (int*int) stream = fn () => ...
+//
+*)
+
+(* ****** ****** *)
+
+#use "./../../../../classlib/OCaml/MyOcaml.ml"
+;;
+
+let theNatPairs: (int*int) stream = fn () =>
+
+
+
+
+
+(* ****** ****** *)
