ee507f5f48434f8e7659211c1cddf6829feb108f
Polina <93955709+minlaux@users.noreply.github.com>
2023-10-31 14:02:32 -0400

    working assign5

--- assigns/assign5/MySolution/OCaml/assign5-MySolution.ml
+++ assigns/assign5/MySolution/OCaml/assign5-MySolution.ml
@@ -14,5 +14,62 @@
 
 *)
 
-let parse (s : string) : expr option = 
 
+(* ****** ****** *)
+
+
+#use "./../../assign5.ml"
+;;
+
+#use "./../../../../classlib/OCaml/MyOcaml.ml"
+;;
+
+
+(*
+main parse function
+takes input string
+returns expr option of input string
+*)
+
+let parse (s: string): expr option = 
+    let cs = string_listize(s) in 
+    parse_expr(cs)
+
+
+(*
+parse Int function
+takes input char 
+returns int option of input char
+*)
+
+let parse_int(c: char): int option =
+    let t = digit_of_char(c) in 
+    if (0 <= t) && (t <= 9) then 
+        Some (Int t) 
+    else 
+        None
+
+
+(*
+parse expr function
+takes input char list 
+returns expr option of input char list
+*)
+
+let parse_expr(cs: char List): expr option = 
+    match cs with 
+    | x :: [] ->                            
+        match parse_int(x) with 
+        | Some i -> Some (Int(i))
+        | None -> None                      
+    | x :: '+' :: xs ->                     
+        match parse_int(x) with 
+        | Some i ->                         
+            match parse_expr(xs) with 
+            | Some r -> Some(Add(i, r))
+            | None -> None                  
+        | None -> None                      
+
+
+
+(* ****** ****** *)
