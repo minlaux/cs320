98b844aeb6fbb8af02627b61f71e373b12c6f0c2
Polina <93955709+minlaux@users.noreply.github.com>
2023-10-31 11:07:44 -0400

    working assign5

---
+++ assigns/assign5/MySolution/OCaml/assign5-MySolution.ml
@@ -0,0 +1,18 @@
+(* Please implement a parse function. When given a valid string according
+   to the grammar, your parse function returns an expr value encoding the
+   expression.
+
+   Example (Accpeted Strings):
+   parse "(add 1 2 3)" = Some (Add [Int 1; Int 2; Int 3])
+   parse "(mul (add 1 2) 3 (mul 1))" = Some (Mul [Add [Int 1; Int 2]; Int 3; Mul [Int 1]])
+
+   Example (Rejected Strings):
+   parse "()" = None
+   parse "(add)" = None
+   parse "(add 1 2))" = None
+   parse "((mul 1 2)" = None
+
+*)
+
+let parse (s : string) : expr option = 
+
