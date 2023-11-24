d1cd33d4a28aa593e5bb72d3a7bc7154600900d5
Polina <93955709+minlaux@users.noreply.github.com>
2023-11-23 21:35:50 -0500

    working interp1

--- assigns/interp1/MySolution/interp1.ml
+++ assigns/interp1/MySolution/interp1.ml
@@ -17,27 +17,27 @@ let interp (s : string) : string list option = (* YOUR CODE *)
 
 #use "./../../../classlib/OCaml/MyOCaml.ml";;
 
-
+(*
 (* 
 grammar: constants 
-*)
+
 ⟨digit⟩ ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 ⟨nat⟩ ::= ⟨digit⟩ | ⟨digit⟩⟨nat⟩
 ⟨int⟩ ::= ⟨nat⟩ | -⟨nat⟩
 ⟨bool⟩ ::= True | False
 ⟨const⟩ ::= ⟨int⟩ | ⟨bool⟩ | Unit
-
+*)
 
 (* 
 grammar: programs 
-*)
+
 ⟨prog⟩ ::= ⟨coms⟩
 ⟨com⟩ ::= Push ⟨const⟩ | Pop | Trace
          | Add | Sub | Mul | Div
          | And | Or | Not
          | Lt | Gt
 ⟨coms⟩ ::= ϵ | ⟨com⟩; ⟨coms⟩
-
+*)
 
 type prog =
    | Push of prog * const
@@ -70,25 +70,39 @@ let rec fetch(s: string)(m: mem): int =
          v 
       else 
          fetch s xs 
-  
 
-let rec eval_step_s (x:expr_v) (m:mem): expr_v =
-  match x with
-  | V v1 -> D (fetch v1 m)
-  | Add (D v1,D v2) -> D(v1+v2)
-  | Sub (D v1,D v2) -> D(v1-v2)
-  | Add (D v1,e2) -> let e2' = eval_step_s e2 m in Add(D v1,e2')
-  | Sub (D v1,e2) -> let e2' = eval_step_s e2 m in Sub(D v1,e2')
-  | Add (V v1,e2) -> let v2 = fetch v1 m  in Add(D v2,e2)
-  | Sub (V v1,e2) -> let v2 = fetch v1 m in Sub(D v2,e2)
+
+let integer: int parser =
+   (let* _ = char '-' in
+   let* x = natural in pure(-x))
+   <|>
+   (let* x = natural in pure x)
+
+
+let bool: prog parser =
+   (let* x = "True" in 
+      pure(B true))
+   <|>
+   (let* x = "False" in 
+      pure (B false))
+
+let parse_const: const parser =
+
+let rec to_string x: string = 
+
 
 
 let rec eval_step(g: prog)(m: mem): prog =
    match g with 
-   | Push c -> 
+   | Push c -> Some list_concat[[c]; mem]
    | Pop -> 
+   | Trace -> 
    | And (B false, _) -> Some (B false)
-   | And (B true, B v2)
+   | And (B true, B v2) -> Some (B v2)
+   | And (B true, e2) -> 
+      (match eval_step e1 with 
+      | None -> None 
+      | Some e1' -> (And (e1', e2)))
 
 
 let rec parse_com(com_str: string): prog =
@@ -110,30 +124,105 @@ let rec parse_com(com_str: string): prog =
 
 let rec to_string(g: prog): string =
    match g with 
-   | And -> ""
-   | And (hd :: tl) ->
-
-
-
-
-    | Add([]) -> "" 
-    | Add(hd :: tl) -> 
-        list_foldleft(tl)("(add" ^ sexpr_to_string hd)
-            (fun acc e -> "(add" ^ sexpr_to_string e) ^ ")"
-    | SMul([]) -> "" 
-    | SMul(hd :: tl) -> 
-        list_foldleft(tl)("(mul" ^ sexpr_to_string hd)
-            (fun acc e -> "(mul" ^ sexpr_to_string e) ^ ")"
-
-
-
-
+   | Int x -> str(char_of_digit x)
+   | B True -> "True"
+   | B False -> "False"
+   | U Unit -> "Unit"
+   | _ -> "Panic" 
 
 
 let interp(s: string): string list option = 
 
-
-
+*)
 
 
 (* ****** ****** *)
+
+
+type const =
+  | CInt of int
+  | CBool of bool
+
+type stack_op =
+  | Push of const
+  | Pop
+  | Trace
+  | Add | Sub | Mul | Div
+  | And | Or | Not
+  | Lt | Gt
+
+type program = stack_op list
+
+let rec parse_const (const_str : string) : const =
+  match const_str with
+  | "True" -> CBool true
+  | "False" -> CBool false
+  | _ ->
+    if String.length const_str > 0 && const_str.[0] = '-'
+    then CInt (-int_of_string (String.sub const_str 1 (String.length const_str - 1)))
+    else CInt (int_of_string const_str)
+
+and parse_com (com_str : string) : stack_op =
+  let tokens = String.split_on_char ' ' com_str in
+  match tokens with
+  | "Push" :: rest -> Push (parse_const (String.concat " " rest))
+  | "Pop" :: _ -> Pop
+  | "Trace" :: _ -> Trace
+  | "Add" :: _ -> Add
+  | "Sub" :: _ -> Sub
+  | "Mul" :: _ -> Mul
+  | "Div" :: _ -> Div
+  | "And" :: _ -> And
+  | "Or" :: _ -> Or
+  | "Not" :: _ -> Not
+  | "Lt" :: _ -> Lt
+  | "Gt" :: _ -> Gt
+  | _ -> failwith "Invalid command"
+
+
+and parse_coms (coms_str : string list) : program =
+  List.map parse_com coms_str
+
+let interpret_program (program : program) : const list option =
+  let rec eval (stack : const list) (ops : stack_op list) : const list option =
+    match ops with
+    | [] -> Some stack
+    | op :: rest ->
+      match op with
+      | Push c -> eval (c :: stack) rest
+      | Pop -> (match stack with _ :: tl -> eval tl rest | _ -> None)
+      | Trace ->
+        (List.map (function CBool b -> string_of_bool b | CInt i -> string_of_int i) stack |> String.concat " " |> print_endline;
+         eval stack rest)
+      | Add | Sub | Mul | Div | And | Or | Not | Lt | Gt ->
+        (* Handle binary operations *)
+        let binary_op (f : int -> int -> int) : const list option =
+          match stack with
+          | CInt a :: CInt b :: tl -> eval (CInt (f b a) :: tl) rest
+          | _ -> None
+        in
+        (match op with
+         | Add -> binary_op (+)
+         | Sub -> binary_op (-)
+         | Mul -> binary_op ( * )
+         | Div -> binary_op (/)
+         | And -> binary_op (fun a b -> if a <> 0 && b <> 0 then 1 else 0)
+         | Or -> binary_op (fun a b -> if a <> 0 || b <> 0 then 1 else 0)
+         | Not -> (match stack with CInt a :: tl -> eval (CInt (if a = 0 then 1 else 0) :: tl) rest | _ -> None)
+         | Lt -> binary_op (fun a b -> if b < a then 1 else 0)
+         | Gt -> binary_op (fun a b -> if b > a then 1 else 0)
+         | _ -> None)
+  in
+  eval [] program
+
+let interp (program_str : string) : string list option =
+  let coms_str = String.split_on_char ';' program_str in
+  let program = parse_coms coms_str in
+  match interpret_program program with
+  | Some result -> Some (List.map (function CBool b -> string_of_bool b | CInt i -> string_of_int i) result)
+  | None -> None
+
+let () =
+  match interp "Push False; Push False; And; Not; Trace; Push False; Not; Push False; Not; Or; Trace;" with
+  | Some result -> List.iter print_endline result
+  | None -> print_endline "Error in program"
